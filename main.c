01f317ebc9268f787cbc9c72a0bfa32a88edb274
Ping-Hung (Nick) Ko <ko.binghong@gmail.com>
2026-03-24 23:38:47 +0800

    added result from 1st experiment

--- main.c
+++ main.c
@@ -5,23 +5,16 @@
 #include "allocator.h"
 #include "list.h"
 
-#define ALLOCATOR /* uncomment this to test allocator code */
+#define ALLOCATOR
 #define TWO_SCAN /* uncomment this for 2 scan benchmarks */
 
 const char *prog_name = "list_creation";
-#define LIST_SIZE 10
-
+#define LIST_SIZE 10000
 
 /* static helper for shuffle */
-static inline void _fisher_yates_shuffle(struct list_node **a_head);
+// static inline void _fisher_yates_shuffle(struct list_node **a_head);
 
 int main() {
-    /* These preprocessor directive are just temporary and can be changed:
-     *  - if *ALLOCATOR* symbol is defined in the program, compile the ifdef
-     *  code block.
-     */
-#ifdef ALLOCATOR
-    /* using memory allocator */
     struct slab_allocator allocator = {.head = NULL};
     allocator_init(&allocator, sizeof(struct list_node), LIST_SIZE);
     struct list_header *list = list_create(&allocator, LIST_SIZE);
@@ -29,63 +22,56 @@ int main() {
     /* shuffle the allocated nodes around to mimic real-world allocation
      * (randomized addresses)*/
     struct list_node *head = list->head;
-    _fisher_yates_shuffle(&head);
-
+    // _fisher_yates_shuffle(&head); /* this tend to have most cache misses: makes
+    //                                  sense due to its "randomized traversal */
 #ifdef TWO_SCAN
     printf("The middle is at %p\n", (void *)find_middle_two_scan(head));
 #else
     printf("The middle is at %p\n", (void *)find_middle_fast_slow(head));
 #endif /* end of TWO_SCAN */
-
     list_free(&allocator, &list);
     allocator_release(&allocator);
-#else
-    /* repeatedly calling malloc */
-    struct list_node *head = list_create(LIST_SIZE);
-    /* maybe do something with the created list */
-    list_free_loop(head);
-#endif /* end of ALLOCATOR */
     return EXIT_SUCCESS;
 }
 
-static inline void _fisher_yates_shuffle(struct list_node **a_head)
-{
-    /* source: https://hackmd.io/@sysprog/c-linked-list?stext=64508%3A842%3A0%3A1774105843%3AeZZpmd */
-    srand(time(NULL));
-
-    // First, we have to know how long is the linked list
-    int len = 0;
-    struct list_node **indirect = a_head;
-    while (*indirect) {
-        len++;
-        indirect = &(*indirect)->next;
-    }   
-
-    // Append shuffling result to another linked list
-    struct list_node *new = NULL;
-    struct list_node **new_head = &new;
-    struct list_node **new_tail = &new;
-
-    while (len) {
-        int random = rand() % len;
-        indirect = a_head;
-
-        while (random--)
-            indirect = &(*indirect)->next;
-
-        struct list_node *tmp = *indirect;
-        *indirect = (*indirect)->next;
-
-        tmp->next = NULL;
-        if (new) {
-            (*new_tail)->next = tmp;
-            new_tail = &(*new_tail)->next;
-        } else {
-            new = tmp;
-        }
-
-        len--;
-    }   
-
-    *a_head = *new_head;
-}
+// static inline void _fisher_yates_shuffle(struct list_node **a_head)
+// {
+//     /* source: https://hackmd.io/@sysprog/c-linked-list?stext=64508%3A842%3A0%3A1774105843%3AeZZpmd */
+//     srand(time(NULL));
+// 
+//     // First, we have to know how long is the linked list
+//     int len = 0;
+//     struct list_node **indirect = a_head;
+//     while (*indirect) {
+//         len++;
+//         indirect = &(*indirect)->next;
+//     }   
+// 
+//     // Append shuffling result to another linked list
+//     struct list_node *new = NULL;
+//     struct list_node **new_head = &new;
+//     struct list_node **new_tail = &new;
+// 
+//     while (len) {
+//         int random = rand() % len;
+//         indirect = a_head;
+// 
+//         while (random--)
+//             indirect = &(*indirect)->next;
+// 
+//         struct list_node *tmp = *indirect;
+//         *indirect = (*indirect)->next;
+// 
+//         tmp->next = NULL;
+//         if (new) {
+//             (*new_tail)->next = tmp;
+//             new_tail = &(*new_tail)->next;
+//         } else {
+//             new = tmp;
+//         }
+// 
+//         len--;
+//     }   
+// 
+//     *a_head = *new_head;
+// }
--- report.md
+++ report.md
@@ -13,6 +13,25 @@ Referring back to [HackMD's Experiment Design](https://hackmd.io/@d1MVB-tCQ92KCH
 4. Run the middle-finding algorithm of choice (*two-scan* or *fast-and-slow-pointers*).
 5. Free all allocated memory.
 
+### Experiment Procedure:
+* 3 trials will be run, each time with different number of `list_node`s.
+* The three numbers are chosen because they are mentioned in the problem.
+* Note that the actual numbers chosen are adjusted to`sizeof(struct list_node)`
+  being 16 bytes so we could allocate exactly $10^4$, $10^6$, and $10^8$ nodes.
+
+* `perf` commands:
+    1. `perf record -e L1-dcache-load-misses,LLC-load-misses`: track L1d cache
+       misses and LLC misses (Last-Level-Cache miss (when CPU *must* fetch
+       RAM))
+    2. `perf report`: for summary
+    3. 
+
+
+
+|trial number|trial 1|trial 2|trial 3|
+|:---:|:---:|:---:|:---:|
+|number of nodes| $10^4$ (160000 bytes) | $10^6$ (16000000 bytes) | $10^8$ (1600000000 bytes)|
+
 ## Experiment Environment
 ### CPU
 ```
@@ -26,18 +45,30 @@ Vendor ID:                   AuthenticAMD
   Model name:                AMD Ryzen 5 3500U with Radeon Vega Mobile Gfx
     CPU family:              23
     Model:                   24
+    Thread(s) per core:      2
 ```
+### Caches
+```
+NAME ONE-SIZE ALL-SIZE WAYS TYPE        LEVEL SETS PHY-LINE COHERENCY-SIZE
+L1d       32K     128K    8 Data            1   64        1             64
+L1i       64K     256K    4 Instruction     1  256        1             64
+L2       512K       2M    8 Unified         2 1024        1             64
+L3         4M       4M   16 Unified         3 4096        1             64
+```
+
+**Summary:** 4 cores, each has a L1d (data cache) of size 32K
+
 ### Operating System
 ```
 Linux IdeaPad-3-15ADA05 6.17.0-19-generic #19~24.04.2-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar  6 23:08:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux
 ```
 
 ## Results
-* 3 trials will be run, each time with different number of `list_node`s.
+### 
+![$10^4$ 2-scan](Screenshot\ from\ 2026-03-24\ 23-36-14.png)
 
-|trial number|trial 1|trial 2|trial 3|
+|number of nodes| $10^4$ (160000 bytes) | $10^6$ (16000000 bytes) | $10^8$ (1600000000 bytes)|
 |:---:|:---:|:---:|:---:|
-|number of nodes| $10^4$ | $10^5$ | $10^6$|
 |performance |  |  |  |
 
 
